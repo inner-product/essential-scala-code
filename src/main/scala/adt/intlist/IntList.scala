10a99f785f1cbb70dda2745e021f5b5a01848f55
Adam Rosien <adam@rosien.net>
2021-03-05 14:03:49 -0800

    Fix compiler warnings about implicit string conversion.

--- src/main/scala/adt/intlist/IntList.scala
+++ src/main/scala/adt/intlist/IntList.scala
@@ -4,11 +4,11 @@ package intlist
 object Main extends App {
   // val ints = IntPair(1, IntPair(2, IntPair(3, IntNil())))
 
-  // println(ints + """.contains(1) == """ + ints.contains(1))
-  // println(ints + """.contains(5) == """ + ints.contains(5))
+  // println(ints.toString + """.contains(1) == """ + ints.contains(1))
+  // println(ints.toString + """.contains(5) == """ + ints.contains(5))
 
-  // println(ints + """.add(1) == """ + ints.add(1))
-  // println(ints + """.add(5) == """ + ints.add(5))
+  // println(ints.toString + """.add(1) == """ + ints.add(1))
+  // println(ints.toString + """.add(5) == """ + ints.add(5))
 
-  // println(ints + """.total == """ + ints.total)
+  // println(ints.toString + """.total == """ + ints.total)
 }
--- src/main/scala/generic/mylist/MyList.scala
+++ src/main/scala/generic/mylist/MyList.scala
@@ -6,19 +6,19 @@ object Main extends App {
   // val ints = MyPair(1, MyPair(3, MyPair(5, MyNil())))
   // val strs = MyPair("foo", MyPair("bar", MyPair("baz", MyNil())))
 
-  // println(ints + """.exists(_ > 1)         == """ + ints.exists(_ > 1))
-  // println(ints + """.filter(_ > 1)         == """ + ints.filter(_ > 1))
-  // println(ints + """.find(_ > 1)           == """ + ints.find(_ > 1))
+  // println(ints.toString + """.exists(_ > 1)         == """ + ints.exists(_ > 1))
+  // println(ints.toString + """.filter(_ > 1)         == """ + ints.filter(_ > 1))
+  // println(ints.toString + """.find(_ > 1)           == """ + ints.find(_ > 1))
 
-  // println(strs + """.exists(_.head == 'b')   == """ + strs.exists(_.head == 'b'))
-  // println(strs + """.filter(_.head == 'b')   == """ + strs.filter(_.head == 'b'))
-  // println(strs + """.find(_.head == 'b')     == """ + strs.find(_.head == 'b'))
+  // println(strs.toString + """.exists(_.head == 'b')   == """ + strs.exists(_.head == 'b'))
+  // println(strs.toString + """.filter(_.head == 'b')   == """ + strs.filter(_.head == 'b'))
+  // println(strs.toString + """.find(_.head == 'b')     == """ + strs.find(_.head == 'b'))
 
   // def add(ints: MyList[Int], num: Int): MyList[Int] =
   //   ???
 
   // println("""add(""" + ints + """, 1))     == """ + add(ints, 1))
-  // println(strs + """.map(_ + "!"))         == """ + strs.map(_ + "!"))
+  // println(strs.toString + """.map(_ + "!"))         == """ + strs.map(_ + "!"))
 
   // def processInt(x: Int): MyList[Int] =
   //   MyPair(x, MyPair(x * 10, MyNil()))
@@ -26,12 +26,12 @@ object Main extends App {
   // def processStr(x: String): MyList[String] =
   //   MyPair(x, MyPair(x + "!", MyNil()))
 
-  // println(ints + """.flatMap(processInt)   == """ + ints.flatMap(processInt))
-  // println(strs + """.flatMap(processStr)   == """ + strs.flatMap(processStr))
+  // println(ints.toString + """.flatMap(processInt)   == """ + ints.flatMap(processInt))
+  // println(strs.toString + """.flatMap(processStr)   == """ + strs.flatMap(processStr))
 
-  // println(ints + """.foldLeft(0)(_ + _))   == """ + ints.foldLeft(0)(_ + _))
-  // println(ints + """.foldRight(0)(_ + _))  == """ + ints.foldRight(0)(_ + _))
+  // println(ints.toString + """.foldLeft(0)(_ + _))   == """ + ints.foldLeft(0)(_ + _))
+  // println(ints.toString + """.foldRight(0)(_ + _))  == """ + ints.foldRight(0)(_ + _))
 
-  // println(strs + """.foldLeft("")(_ + _))  == """ + strs.foldLeft("")(_ + _))
-  // println(strs + """.foldRight("")(_ + _)) == """ + strs.foldRight("")(_ + _))
+  // println(strs.toString + """.foldLeft("")(_ + _))  == """ + strs.foldLeft("")(_ + _))
+  // println(strs.toString + """.foldRight("")(_ + _)) == """ + strs.foldRight("")(_ + _))
 }
